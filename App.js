ef85e519019d1dba088c8efde0fac7a4c5002b0c
Jempsa <t2koje02@students.oamk.fi>
2024-09-06 11:24:45 +0300

    modaali

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Alert, Modal, StyleSheet, Text, Pressable, View} from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [VisibleModal, setModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style={styles.centeredView}>
+      <Modal
+        transparent={true}
+        visible={VisibleModal}
+
+        onRequestClose={() => {
+          Alert.alert('U Succesfully hide it bro.');
+          setModalVisible(!VisibleModal);
+        }}>
+
+        <View style={styles.centeredView}>
+          
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Whoaa u idiot. U have been hacked now!!!!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!VisibleModal)}>
+              <Text style={styles.textStyle}>Hide it!!!!</Text>
+            </Pressable>
+          </View>
+
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}>
+        <Text style={styles.textStyle}>Press This button</Text>
+      </Pressable>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  centeredView: {
+    flex: 10,
     justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  
   },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'purple',
+    padding: 35,
+    alignItems: 'center',
+  },
+
+  button: {
+    padding: 10,
+    elevation: 2,
+  },
+
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+
 });
+
+export default App;
